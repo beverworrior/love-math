1f0f69498971f3e897e7d8836f44f40554818833
Jan-Robert Nielsen <robertnielsen33@gmail.com>
2024-02-25 08:01:47 +0000

    added enventlistner and cliked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,47 @@
-function rungame {
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for ( let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+             } else {
+                let gametype = this.getAttribute("data-type");
+                alert(`You clicked ${gametype}`);
+             }
+        })
+    }
+})
+
+function runGame() {
 
 }
 
-function checkAnswer {
+function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer {
+function calculateCorrectAnswer() {
 
 }
 
-function incrementScore{
+function incrementScore() {
 
 }
 
-function incrementWrongAnswer {
+function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion{
+function displayAdditionQuestion() {
     
 }
 
-function displaySubtractQuestion{
+function displaySubtractQuestion() {
     
 }
 
-function displayMultiplyQuestion{
+function displayMultiplyQuestion() {
     
 }
